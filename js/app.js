3b9ebf2c1c7ad243670e5f466dac154c8bcff157
Katarzyna Elliott-Maksymowicz <maksymowicz.katarzyna@gmail.com>
2018-08-06 14:41:17 -0400

    adds function hideCards to hide mismatched pairs

--- js/app.js
+++ js/app.js
@@ -89,14 +89,30 @@ function lockCards() {
 }
 
 
+// if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+
+function hideCards() {
+  const openCards = document.querySelectorAll('div.open');
+  openCards.forEach( function(currentValue, currentIndex, listObj) {
+    currentValue.classList.remove("open");
+    currentValue.classList.remove("show");
+    openCardCont.pop();
+    openCardCont.pop();
+  }
+}
+
+
+
+
+
 /*
 
 
 
 *  - if the list already has another card, check to see if the two cards match
-*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
 
-*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+
+
 
 *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
 
