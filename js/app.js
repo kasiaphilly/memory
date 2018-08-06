11ec4051f19056f3dc4aaec57c185c8e0c07e5f4
Katarzyna Elliott-Maksymowicz <maksymowicz.katarzyna@gmail.com>
2018-08-06 13:18:56 -0400

    adds addOpen function

--- js/app.js
+++ js/app.js
@@ -37,13 +37,19 @@ const myDeck = document.querySelector(".deck");
     let clickCount=0;
 
 
-// set up the event listener for a card.
-
+// set reference to all cards
 const myCards = document.querySelectorAll('div.card');
 
+// function adding "open" class to clicked card
+function addOpen(card) {
+  card.classList.add("open");
+}
 
+// set up the event listener for a card.
 myCards.forEach( function(currentValue, currentIndex, listObj) {
-currentValue.addEventListener("click", function () {
+currentValue.addEventListener("click", function (e) {
+    e.preventDefault();
+    addOpen(currentValue);
     console.log("listening to this click!");
     clickCount++;
     console.log(clickCount);
@@ -51,12 +57,23 @@ currentValue.addEventListener("click", function () {
 });
 
 
-// later add addOpen() instead of console.log
-
-
 
 /*  - display the card's symbol (put this functionality in another function that you call from this one)
+
+
+
+
+
 *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+
+function addOpen() {
+  myClick.classList.add("open");
+};
+
+
+
+
+
 *  - if the list already has another card, check to see if the two cards match
 *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
 *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
