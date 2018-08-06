1b56c645d73711c475c653e1231a4cf55a3f7fa3
Katarzyna Elliott-Maksymowicz <maksymowicz.katarzyna@gmail.com>
2018-08-06 14:15:47 -0400

    adds function storing clicked image

--- js/app.js
+++ js/app.js
@@ -50,14 +50,24 @@ function showSymbol(card) {
   card.classList.add("show");
 }
 
+// container for comparing open cards
+const openCardCont = [];
+
+//function adding class of clicked card to openCardCont
+function storeImage(card) {
+  const currentImage = card.firstElementChild.className;
+  openCardCont.push(currentImage);
+}
+
 // set up the event listener for a card.
 myCards.forEach( function(currentValue, currentIndex, listObj) {
 currentValue.addEventListener("click", function (e) {
     e.preventDefault();
+    clickCount++;
     showSymbol(currentValue);
     addOpen(currentValue);
+    storeImage(currentValue);
     console.log("listening to this click!");
-    clickCount++;
     console.log(clickCount);
   });
 });
@@ -67,21 +77,16 @@ currentValue.addEventListener("click", function (e) {
 
 
 
-
-
-/*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-
-function addOpen() {
-  myClick.classList.add("open");
-};
-
-
+/*
 
 
 
 *  - if the list already has another card, check to see if the two cards match
 *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+
 *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+
 *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+
 *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 */
